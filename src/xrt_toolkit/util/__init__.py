b058be0349941c0d804a768ea968a00b3beddd97
Sepand KASHANI <sepand@kashani.ch>
2025-04-24 08:07:24 +0200

    [util] Auto-import low-level tools.

---
+++ src/xrt_toolkit/util/__init__.py
@@ -0,0 +1,13 @@
+from .dtype import (
+    TranslateDType as TranslateDType,
+)
+from .misc import (
+    broadcast_seq as broadcast_seq,
+)
+from .mesh import (
+    UniformSpec as UniformSpec,
+)
+from .bbox import (
+    bbox_contains as bbox_contains,
+    ray_bbox_intersect as ray_bbox_intersect,
+)
