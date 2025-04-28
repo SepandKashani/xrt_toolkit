f6b66827e5886b543c160aae957a63e5cc25b784
Sepand KASHANI <sepand@kashani.ch>
2025-04-28 20:33:42 +0200

    [struct_xrt] xrt_struct_adjoint(): tentative.

--- src/xrt_toolkit/struct_xrt.py
+++ src/xrt_toolkit/struct_xrt.py
@@ -5,7 +5,7 @@ import drjit as dr
 
 import xrt_toolkit.util as xrtu
 
-from .ray_xrt import xrt_apply
+from .ray_xrt import xrt_adjoint, xrt_apply
 
 ArrayNNfT = typ.TypeVar("ArrayNNfT", bound=dr.AnyArray)
 FloatT = typ.TypeVar("FloatT", bound=dr.AnyArray)
@@ -140,6 +140,117 @@ def xrt_struct_apply(
             data=data,
         )
         dr.scatter(buffer, proj, index)
+
+        i += 1
+        index += L_proj
+
+    return buffer
+
+
+@dr.syntax
+def xrt_struct_adjoint(
+    ray_spec: RaySpecT,
+    knot_spec: xrtu.UniformSpec,
+    order: int,
+    data: FloatT,
+    buffer: FloatT = None,
+) -> FloatT:
+    r"""
+    Compute 2D/3D structured back-projections.
+
+    Adjoint of ``xrt_struct_apply()``: maps projection weights to volume expansion coefficients.
+
+    Parameters
+    ----------
+    ray_spec: tuple[ArrayNNfT, ArrayNNfT, UniformSpec]
+        (L,) implicit ray descriptors :math:`\bbt \in \bR^{D}` and directions :math:`\bbn \in \bR^{D}`.
+
+        Expected arguments are:
+
+        * ray_t_spec: ArrayNNfT
+          (N_proj, D, D) homogeneous transforms :math:`\bbH_{t} = [\bbA_{t} \in \bR^{D \times (D-1)}, \bbb_{t} \in \bR^{D}]`.
+        * ray_d_spec: ArrayNNfT
+          (N_proj, D, D) homogeneous transforms :math:`\bbH_{d} = [\bbA_{d} \in \bR^{D \times (D-1)}, \bbb_{d} \in \bR^{D}]`.
+        * ray_u_spec: UniformSpec
+          D-dim uniform mesh.
+
+        Let :math:`\bbu \in \bR^{D}` be a point on the mesh specified by `ray_u_spec`.
+        Then `ray_spec` encodes rays of the form
+
+        .. math::
+
+           \bbt(\bbu) = \bbH_{t} \bbu,
+           \bbd(\bbu) = \bbH_{d} \bbu.
+
+        `ray_spec` encodes ``L = N_proj * prod(ray_u_spec.num)`` projections.
+    knot_spec: UniformSpec
+        Volume properties :math:`(\bbx_{0}, \bbDelta, \bbQ)`.
+    order: 0 | 1
+        Data interpolation order.
+
+        This parameter sets which :math:`\psi` is used to interpolate data values.
+    data: FloatT
+        (L,) projections :math:`g_{l} \in \bR`.
+        The (N_proj, *ray_u_spec.num) mesh-points are serialized in C-order.
+    buffer: FloatT
+        (Q1,...,QD) flattened buffer in which to accumulate back-projected weights :math:`f_{\bbq} \in \bR`.
+
+    Returns
+    -------
+    b_proj: FloatT
+        (Q1,...,QD) flattened C-ordered back-projected weights :math:`f_{\bbq} \in \bR`.
+    """
+    ray_t_spec, ray_d_spec, ray_u_spec = ray_spec
+
+    ArrayNNf = type(ray_t_spec)
+    ArrayNf = dr.value_t(ArrayNNf)
+    Float = dr.value_t(ArrayNf)
+    UInt = dr.value_t(dr.uint32_array_t(ArrayNf))
+
+    # type checking ---------------------------------------
+    D = dr.size_v(ArrayNf)
+    assert (ray_t_spec.ndim == 3) and (D in (2, 3))
+    assert type(ray_d_spec) is ArrayNNf
+    assert ray_u_spec.ndim == D
+
+    assert knot_spec.ndim == D
+    assert order in (0, 1)
+
+    assert (N_proj := ray_t_spec.shape[-1]) == ray_d_spec.shape[-1]
+    L_proj = math.prod(ray_u_spec.num)
+    L = N_proj * L_proj
+    assert type(data) is Float
+    assert len(data) == L
+
+    if buffer is None:
+        buffer = dr.zeros(Float, math.prod(knot_spec.num))
+    else:
+        assert type(buffer) is Float
+        assert len(buffer) == math.prod(knot_spec.num)
+    # -----------------------------------------------------
+
+    u = [None] * D
+    for d, (start, step, num) in enumerate(ray_u_spec):
+        u[d] = start + step * dr.arange(Float, num)
+    uu = ArrayNf(*dr.meshgrid(*u, indexing="ij"))
+
+    i = UInt(0)
+    index = dr.arange(UInt, 0, L_proj)
+    while i < N_proj:
+        H_t = dr.gather(ArrayNNf, ray_t_spec, i)
+        ray_t = H_t @ uu
+
+        H_d = dr.gather(ArrayNNf, ray_d_spec, i)
+        ray_d = H_d @ uu
+
+        proj = dr.gather(Float, data, index)
+        xrt_adjoint(
+            ray_spec=(ray_t, ray_d),
+            knot_spec=knot_spec,
+            order=order,
+            data=proj,
+            buffer=buffer,
+        )
 
         i += 1
         index += L_proj
