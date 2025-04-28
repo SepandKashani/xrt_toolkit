3b5417e2f47b991b11396463f66454b44d9e942e
Sepand KASHANI <sepand@kashani.ch>
2025-04-28 20:14:25 +0200

    [struct_xrt] xrt_struct_apply(): tentative.

---
+++ src/xrt_toolkit/struct_xrt.py
@@ -0,0 +1,145 @@
+import math
+import typing as typ
+
+import drjit as dr
+
+import xrt_toolkit.util as xrtu
+
+from .ray_xrt import xrt_apply
+
+ArrayNNfT = typ.TypeVar("ArrayNNfT", bound=dr.AnyArray)
+FloatT = typ.TypeVar("FloatT", bound=dr.AnyArray)
+RaySpecT = tuple[ArrayNNfT, ArrayNNfT, xrtu.UniformSpec]
+
+
+@dr.syntax
+def xrt_struct_apply(
+    ray_spec: RaySpecT,
+    knot_spec: xrtu.UniformSpec,
+    order: int,
+    data: FloatT,
+    buffer: FloatT = None,
+) -> FloatT:
+    r"""
+    Compute 2D/3D structured projections.
+
+    Let :math:`f: \bR^{D} \to \bR` such that
+
+    .. math::
+
+       f(\bbx)
+       =
+       \sum_{\bbq \in \discreteRange{\bbZero, \bbQ-1}}
+       f_{\bbq} \psi_(\bbx - \bbx_{\bbq}),
+
+    with
+
+    .. math::
+
+       f_{\bbq} \in \bR,
+       \bbx_{q} = \bbx_{0} + \bbq \odot \bbDelta,
+           \bbx_{0} \in \bR^{D},
+           \bbDelta \in \bR_{+}^{D},
+       \psi(\bbx) = either
+           \prod_{d=1}^{D}                                     1_[-\bbDelta_{d} / 2, \bbDelta_{d} / 2](\bbx_{d}),
+           \prod_{d=1}^{D} (1 - \abs{\bbx_{d} / \bbDelta_{d}}) 1_[-\bbDelta_{d}    , \bbDelta_{d}    ](\bbx_{d}).
+
+    Then ``xrt_struct_apply()`` computes samples of
+
+    .. math::
+
+       \xrt[f](\bbn, \bbt) = \int_{\bR} f(\bbt + \alpha \bbn) d\alpha
+
+    Parameters
+    ----------
+    ray_spec: tuple[ArrayNNfT, ArrayNNfT, UniformSpec]
+        (L,) implicit ray descriptors :math:`\bbt \in \bR^{D}` and directions :math:`\bbn \in \bR^{D}`.
+
+        Expected arguments are:
+
+        * ray_t_spec: ArrayNNfT
+          (N_proj, D, D) homogeneous transforms :math:`\bbH_{t} = [\bbA_{t} \in \bR^{D \times (D-1)}, \bbb_{t} \in \bR^{D}]`.
+        * ray_d_spec: ArrayNNfT
+          (N_proj, D, D) homogeneous transforms :math:`\bbH_{d} = [\bbA_{d} \in \bR^{D \times (D-1)}, \bbb_{d} \in \bR^{D}]`.
+        * ray_u_spec: UniformSpec
+          D-dim uniform mesh.
+
+        Let :math:`\bbu \in \bR^{D}` be a point on the mesh specified by `ray_u_spec`.
+        Then `ray_spec` encodes rays of the form
+
+        .. math::
+
+           \bbt(\bbu) = \bbH_{t} \bbu,
+           \bbd(\bbu) = \bbH_{d} \bbu.
+
+        `ray_spec` encodes ``L = N_proj * prod(ray_u_spec.num)`` projections.
+    knot_spec: UniformSpec
+        Volume properties :math:`(\bbx_{0}, \bbDelta, \bbQ)`.
+    order: 0 | 1
+        Data interpolation order.
+
+        This parameter sets which :math:`\psi` is used to interpolate data values.
+    data: FloatT
+        (Q1,...,QD) flattened C-ordered volume weights :math:`f_{\bbq} \in \bR`.
+    buffer: FloatT
+        (L,) buffer in which to accumulate projections.
+        The (N_proj, *ray_u_spec.num) mesh-points are serialized in C-order.
+
+    Returns
+    -------
+    proj: FloatT
+        (L,) projections :math:`\xrt[f] \in \bR`.
+    """
+    ray_t_spec, ray_d_spec, ray_u_spec = ray_spec
+
+    ArrayNNf = type(ray_t_spec)
+    ArrayNf = dr.value_t(ArrayNNf)
+    Float = dr.value_t(ArrayNf)
+    UInt = dr.value_t(dr.uint32_array_t(ArrayNf))
+
+    # type checking ---------------------------------------
+    D = dr.size_v(ArrayNf)
+    assert (ray_t_spec.ndim == 3) and (D in (2, 3))
+    assert type(ray_d_spec) is ArrayNNf
+    assert ray_u_spec.ndim == D
+
+    assert knot_spec.ndim == D
+    assert order in (0, 1)
+
+    assert type(data) is Float
+    assert len(data) == math.prod(knot_spec.num)
+
+    assert (N_proj := ray_t_spec.shape[-1]) == ray_d_spec.shape[-1]
+    L_proj = math.prod(ray_u_spec.num)
+    L = N_proj * L_proj
+    if buffer is None:
+        buffer = dr.zeros(Float, L)
+    else:
+        assert type(buffer) is Float
+        assert len(buffer) == L
+    # -----------------------------------------------------
+
+    u = [start + step * dr.arange(Float, num) for (start, step, num) in ray_u_spec]
+    uu = ArrayNf(*dr.meshgrid(*u, indexing="ij"))
+
+    i = UInt(0)
+    index = dr.arange(UInt, 0, L_proj)
+    while i < N_proj:
+        H_t = dr.gather(ArrayNNf, ray_t_spec, i)
+        ray_t = H_t @ uu
+
+        H_d = dr.gather(ArrayNNf, ray_d_spec, i)
+        ray_d = H_d @ uu
+
+        proj = xrt_apply(
+            ray_spec=(ray_t, ray_d),
+            knot_spec=knot_spec,
+            order=order,
+            data=data,
+        )
+        dr.scatter(buffer, proj, index)
+
+        i += 1
+        index += L_proj
+
+    return buffer
