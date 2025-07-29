39a60c7ab1b50b53b07008cd895c1ed25d3fb636
Sepand KASHANI <sepand@kashani.ch>
2025-07-29 09:21:38 +0200

    [util] Add type traits.

--- src/xrt_toolkit/util/__init__.py
+++ src/xrt_toolkit/util/__init__.py
@@ -10,3 +10,8 @@ from .mesh import (
 from .compat import (
     asarray as asarray,
 )
+from .type_traits import (
+    float_array_t as float_array_t,
+    int_array_t as int_array_t,
+    uint_array_t as uint_array_t,
+)
